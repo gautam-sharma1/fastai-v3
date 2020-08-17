1389374120f9e2c6702bce5748f2b7610abd6492
Gautam Sharma <55221455+gautam-sharma1@users.noreply.github.com>
2020-08-16 20:11:17 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,13 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1NRRlNEIU2_n-2B4uwbmjG6XuvQmE0ogW'
 export_file_name = 'stage-1.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ["Speed limit (20km/h)","Speed limit (30km/h)", "Speed limit (50km/h)", "Speed limit (60km/h)","Speed limit (70km/h)","Speed limit (80km/h)",
+"End of speed limit (80km/h)","Speed limit (100km/h)", "Speed limit (120km/h)", "No passing", "No passing for vechiles over 3.5 metric tons" ,
+"Right-of-way at the next intersection" , "Priority road", "Yield" , "Stop" , "No vechiles", "Vechiles over 3.5 metric tons prohibited", "No entry", "General caution",
+"Dangerous curve to the left", "Dangerous curve to the right","Double curve","Bumpy road","Slippery road","Road narrows on the right"
+,"Road work", "Traffic signals","Pedestrians","Children crossing","Bicycles crossing","Beware of ice/snow","Wild animals crossing"
+,"End of all speed and passing limits","Turn right ahead","Turn left ahead","Ahead only","Go straight or right","Go straight or left","Keep right"
+,"Keep left","Roundabout mandatory","End of no passing","End of no passing by vechiles over 3.5 metric tons"]
 path = Path(__file__).parent
 
 app = Starlette()
